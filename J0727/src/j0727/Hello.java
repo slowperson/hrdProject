f1ca864022922d33a38bb98e9d7c419c273064e9
현정 <@DESKTOP-5UMJG37>
2021-07-27 11:26:45 +0900

    hrd project2

--- J0727/src/j0727/Hello.java
+++ J0727/src/j0727/Hello.java
@@ -3,7 +3,8 @@ package j0727;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Helloworld!!");
+		System.out.println("Helloworld!!!");
+		System.out.println("헬로월드!!!");
 
 	}
 
